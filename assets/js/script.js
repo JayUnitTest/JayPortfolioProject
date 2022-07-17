c338e069939b9998654a4d535c1b3f2dda786877
Jay Singh <j.rathore439@gmail.com>
2022-07-17 18:49:57 +0100

    Third- * Slight layout revamp * Colour scheme updated * added a cv button * scroll reveal added * removed about page (keeping single page app, reduced load) * placeholders updated with more placeholders ! *responsiveness much better!
    =====fixings=======
    * need to fix skills section- slide not showing css and js images
    * burger menu needs to be implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,57 @@
-// arrow scroll up functionality 
+const header = document.querySelector("header");
+
 const scrollUp = document.querySelector("#scroll-up");
 
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 0);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navList = document.querySelector('.navList');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navList.classList.toggle('active');
+};
+//hover function over logo- changes from black to pink on hover.. not functioning 
+function hover(pageLogo) {
+    element.setAttribute('src', '/assets/images/New Project.svg');
+  }
+  function unhover(pageLogo) {
+    element.setAttribute('src', '/assets/images/alogo-2.svg');
+  }
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navList.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+	distance: '45px',
+	duration: 2700,
+	reset: true
+})
 
 //swiper testimonial 
-const swiper = new Swiper(".mySwiper", {
-  pagination: {
-  el: ".swiper-pagination",
-  },
-});
+const swiper = new Swiper('.swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    
+  });
 
-addEventListener('scroll', e => {
-  const scrollTop = doc.scrollTop;
-  if(scrollTop > start && scrollTop < stop){
-    const d =scrollTop - start;
-    const scale = Math.max(2,2 - (d) / 500, 1);
-    aboutmeimg.style.transform= `scale(${scale})`
-  }
-})
+
+  scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+
+
+sr.reveal('.home-text',{delay:300, origin:'left'})
+sr.reveal('.home-img',{delay:300, origin:'right'})
+
+sr.reveal('.additional,.about,.portfolio,.skills,.contacts',{delay:200, origin:'bottom'})
